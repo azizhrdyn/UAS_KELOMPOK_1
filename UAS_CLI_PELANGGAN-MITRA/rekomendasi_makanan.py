ca20a1bba764c455c51d378348e2715331b17630
azizhrdyn <azizhardiyan@gmail.com>
2025-12-13 21:51:29 +0700

    Update rekomendasi_makanan.py

--- UAS_CLI_PELANGGAN-MITRA/rekomendasi_makanan.py
+++ UAS_CLI_PELANGGAN-MITRA/rekomendasi_makanan.py
@@ -8,3 +8,11 @@ def rekomendasi_kalori(max_kalori=300):
 def rekomendasi_harga(max_harga=10000):
     df = load_makanan()
     return df[df['harga'] <= max_harga]
+
+def rekomendasi_terbaik(jumlah=3):
+    df = load_makanan().copy()
+    if df.empty:
+        return df
+    df['skor'] = df['harga'].astype(float)*0.7 + df['kalori'].astype(float)*0.3
+    df_sorted = df.sort_values('skor').head(jumlah)
+    return df_sorted.drop(columns=["skor"])
